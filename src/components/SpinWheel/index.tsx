1dde8d0c5db9fb4e78e1186d7745c7d991dbc8c4
Ta Cuong <cuongvt@appota.com>
2024-08-28 11:11:17 +0700

    Format code

--- src/components/SpinWheel/index.tsx
+++ src/components/SpinWheel/index.tsx
@@ -1,122 +1,123 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 
 import styles from "./index.module.scss";
 
 const SpinWheel = () => {
-  const items = ["cat", "dog", "duck", "chicken"];
+  const items: string[] = ["cat", "cow", "dog", "duck", "sheep", "chicken"];
 
-  let speed = 0;
-  let pause = false;
-  let currentDeg = 0;
-  let itemDegs: any = {};
-  let step = 360 / items.length;
+  let speed: number = 0;
+  let pause: boolean = false;
+  let currentDeg: number = 0;
+  const step: number = 360 / items.length;
+  let maxRotation: number =
+    Math.floor(Math.random() * (360 * 6 - 360 * 3 + 1)) + 360 * 3;
+  let itemDegs: {
+    [key in (typeof items)[number]]: { endDeg: number; startDeg: number };
+  } = {};
+  const colors: { b: number; g: number; r: number }[] = items.map(() => ({
+    b: Math.floor(Math.random() * 256),
+    g: Math.floor(Math.random() * 256),
+    r: Math.floor(Math.random() * 256),
+  }));
 
-  const canvasRef = useRef<any>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const toRad = (deg: number) => {
-    return deg * (Math.PI / 180.0);
-  };
+  const toRad = (deg: number) => deg * (Math.PI / 180.0);
 
-  const easeOutSine = (x: number) => {
-    return Math.sin((x * Math.PI) / 2);
-  };
+  const drawWheel = () => {
+    if (!canvasRef.current) return;
 
-  const getPercent = (input: number, min: number, max: number) => {
-    return ((input - min) * 100) / (max - min) / 100;
-  };
-
-  const randomRange = (min: number, max: number) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  let maxRotation = randomRange(360 * 3, 360 * 6);
-
-  const draw = () => {
-    const canvas = canvasRef.current;
-
-    const { width, height } = canvas;
-
-    const radius = width / 2;
-    const centerX = width / 2;
-    const centerY = height / 2;
-    const ctx = canvas.getContext("2d");
-
-    ctx.beginPath();
-    ctx.arc(centerX, centerY, radius, toRad(0), toRad(360));
-    ctx.fillStyle = `rgb(${33},${33},${33})`;
-    ctx.lineTo(centerX, centerY);
-    ctx.fill();
+    const ctx = canvasRef.current.getContext("2d");
+    if (!ctx) return;
 
     let startDeg = currentDeg;
+    const centerX = canvasRef.current.width / 2;
+    const centerY = canvasRef.current.height / 2;
+
     for (let i = 0; i < items.length; i++, startDeg += step) {
-      let endDeg = startDeg + step;
+      const color = colors[i];
+      const endDeg = startDeg + step;
 
       ctx.beginPath();
-      ctx.arc(centerX, centerY, radius - 2, toRad(startDeg), toRad(endDeg));
+      ctx.arc(centerX, centerY, centerX - 2, toRad(startDeg), toRad(endDeg));
+      ctx.fillStyle = `rgb(${color.r - 30},${color.g - 30},${color.b - 30})`;
       ctx.lineTo(centerX, centerY);
       ctx.fill();
 
       ctx.beginPath();
-      ctx.arc(centerX, centerY, radius - 30, toRad(startDeg), toRad(endDeg));
+      ctx.arc(centerX, centerY, centerX - 30, toRad(startDeg), toRad(endDeg));
+      ctx.fillStyle = `rgb(${color.r},${color.g},${color.b})`;
       ctx.lineTo(centerX, centerY);
       ctx.fill();
 
       ctx.save();
+
+      ctx.textAlign = "center";
+      ctx.fillStyle = "#000000";
+      ctx.font = "bold 24px serif";
       ctx.translate(centerX, centerY);
       ctx.rotate(toRad((startDeg + endDeg) / 2));
       ctx.fillText(items[i], 130, 10);
+
       ctx.restore();
 
-      itemDegs[items[i]] = { endDeg: endDeg, startDeg: startDeg };
-
       if (
-        startDeg % 360 < 360 &&
-        startDeg % 360 > 270 &&
+        endDeg % 360 > 0 &&
         endDeg % 360 < 90 &&
-        endDeg % 360 > 0
+        startDeg % 360 > 270 &&
+        startDeg % 360 < 360
       ) {
         console.log(items[i]);
       }
+
+      itemDegs[items[i]] = { endDeg: endDeg, startDeg: startDeg };
     }
-  };
-
-  const createWheel = () => {
-    step = 360 / items.length;
-
-    draw();
   };
 
   const animate = () => {
     if (pause) return;
 
-    speed = easeOutSine(getPercent(currentDeg, maxRotation, 0)) * 20;
+    const percent =
+      ((currentDeg - maxRotation) * 100) / (0 - maxRotation) / 100;
+
+    speed = Math.sin((percent * Math.PI) / 2) * 20;
+
     if (speed < 0.01) {
       speed = 0;
       pause = true;
     }
+
     currentDeg += speed;
-    draw();
+
+    drawWheel();
+
     window.requestAnimationFrame(animate);
   };
 
   const spin = () => {
     if (speed !== 0) return;
 
-    createWheel();
+    currentDeg = 0;
+    maxRotation = 0;
+
+    drawWheel();
 
     const randomResult = items[Math.floor(Math.random() * items.length)];
 
     maxRotation = 360 * 6 - itemDegs[randomResult].endDeg + 10;
 
+    itemDegs = {};
     pause = false;
 
     window.requestAnimationFrame(animate);
   };
 
+  useEffect(() => drawWheel());
+
   return (
     <div className={styles["spin-wheel-container"]}>
       <div className={styles["wheel"]}>
-        <canvas id="canvas" width="500" height="500" ref={canvasRef} />
+        <canvas width="500" height="500" ref={canvasRef} />
         <div onClick={spin} className={styles["center-circle"]}>
           <div className={styles["triangle"]} />
         </div>
